f6ebe8fabd55d09ba72c37a260b6f24a00b5c34c
julian rubio <rubiojulian917@gmail.com>
2024-04-03 16:10:39 -0300

    acordeon listo

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+/*const accordion = document.getElementsByClassName('contentBx');
+for ( i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+    });
+}
+
 $(document).ready(function(){
-    $("#flip").click(function(){
-      $("#panel").slideToggle("slow");
+    $(".label").click(function(){
+      $(".content").slideToggle("slow");
     });
     console.log("has clickeado")
-});
+});*/
+$("h1").on('click', function() {
+    $(this).next("p").slideToggle('slow');
+  });
+  
+  $(document).ready(function() {
+    $("div").children("p").hide();
+  })
